0f5dd766f5c2a0f4c064f0071bb884c2b337b7f0
Sergio Sanhueza <sergio.sergio.98@hotmail.com>
2021-02-01 10:52:50 -0300

    menu desplegable, diseño completo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("todo bien")
 
 const NavButton = document.getElementById("nav-menu-button")
+const NavMenu = document.getElementById("nav-menu")
 let menuOpen = false
 NavButton.addEventListener('click', togglemenu)
 
@@ -9,10 +10,12 @@ function togglemenu(event) {
     if(!menuOpen) {
         console.log("abrir menu")
         NavButton.classList.replace("close","open")
+        NavMenu.classList.replace("close","open")
         menuOpen = true;
     } else {
         console.log("cerrar menu")
         NavButton.classList.replace("open","close")
+        NavMenu.classList.replace("open","close")
         menuOpen = false;
     }
 
