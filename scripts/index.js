e9b953bf907e83ee1f1f83d4af0720303e54ccce
jito-jito <sergio.sergio.98@hotmail.com>
2021-05-13 11:22:01 -0400

    listo par subir

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,17 @@
-console.log("todo bien")
-
 const NavButton = document.getElementById("nav-menu-button")
 const NavMenu = document.getElementById("nav-menu")
 let menuOpen = false
 NavButton.addEventListener('click', togglemenu)
 
 function togglemenu(event) {
-    console.log(event)
 
     if(!menuOpen) {
-        console.log("abrir menu")
         NavButton.classList.replace("close","open")
         NavMenu.classList.replace("close","open")
         menuOpen = true;
     } else {
-        console.log("cerrar menu")
         NavButton.classList.replace("open","close")
         NavMenu.classList.replace("open","close")
         menuOpen = false;
     }
-
-
-}
-
-function funcion () {
-    alert("???")
 }
