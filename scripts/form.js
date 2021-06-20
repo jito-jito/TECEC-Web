b4256b2a2990911eabf562ec7227614b907e2236
jito <sergio.sergio.98@hotmail.com>
2021-06-19 22:54:35 -0400

    mejoras en el código de formulario

--- scripts/form.js
+++ scripts/form.js
@@ -6,6 +6,6 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event) {
     event.preventDefault()
     const form = new FormData(this)
-    $buttonMailto.setAttribute('href', `mailto:tececservicios@gmail.com?subject=nombre: ${form.get('nombre')}  correo: ${form.get('email')}&body=Asunto: ${form.get('asunto')} Mensaje: ${form.get('mensaje')}`)
+    $buttonMailto.setAttribute('href', `mailto:tececservicios@gmail.com?subject=${form.get('nombre')} Asunto:${form.get('asunto')}&body=Correo: ${form.get('email')} Mensaje: ${form.get('mensaje')}`)
     $buttonMailto.click()
 }
